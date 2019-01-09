410dde7485b83bdb8656f6fca46876d3314fb070
yongjunpark <yongjunpark@smilegate.com>
2019-01-09 17:18:55 +0900

    modify create default account point (using event listener: ApplicationReadyEvent), modify readme doc

--- src/main/java/io/github/rxcats/springbootthymeleafdemo/SpringBootThymeleafDemoApplication.java
+++ src/main/java/io/github/rxcats/springbootthymeleafdemo/SpringBootThymeleafDemoApplication.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.slf4j.Slf4j;
 
 import io.github.rxcats.springbootthymeleafdemo.security.entity.Role;
 import io.github.rxcats.springbootthymeleafdemo.security.entity.User;
 import io.github.rxcats.springbootthymeleafdemo.security.repository.RoleRepository;
 import io.github.rxcats.springbootthymeleafdemo.security.repository.UserRepository;
 
+@Slf4j
 @EnableMongoRepositories
 @SpringBootApplication
 public class SpringBootThymeleafDemoApplication {
@@ -34,34 +37,38 @@ public class SpringBootThymeleafDemoApplication {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
+    // create default admin account
+    @EventListener(ApplicationReadyEvent.class)
+    public void checkDefaultAdminAccount() {
 
-            // create default admin account
+        log.info("application ready!");
 
-            Role adminRole = roleRepository.findByRole("ADMIN");
-            if (adminRole == null) {
-                adminRole = new Role();
-                adminRole.setRole("ADMIN");
-                adminRole = roleRepository.save(adminRole);
-            }
+        Role adminRole = roleRepository.findByRole("ADMIN");
+        if (adminRole == null) {
+            adminRole = new Role();
+            adminRole.setRole("ADMIN");
+            adminRole = roleRepository.save(adminRole);
 
-            List<Role> roles = new ArrayList<>();
-            roles.add(adminRole);
+            log.info("created default adminRole.");
+        }
 
-            User user = userRepository.findByEmail("admin@c.com");
-            if (user == null) {
-                user = new User();
-                user.setEmail("admin@c.com");
-                user.setName("admin");
-                user.setPassword(passwordEncoder.encode("admin"));
-                user.setRoles(roles);
-                user.setActive(1);
-                user.setCreatedAt(LocalDateTime.now());
-                user.setUpdatedAt(LocalDateTime.now());
-                userRepository.save(user);
-            }
-        };
+        List<Role> roles = new ArrayList<>();
+        roles.add(adminRole);
+
+        User user = userRepository.findByEmail("admin@c.com");
+        if (user == null) {
+            user = new User();
+            user.setEmail("admin@c.com");
+            user.setName("admin");
+            user.setPassword(passwordEncoder.encode("admin"));
+            user.setRoles(roles);
+            user.setActive(1);
+            user.setCreatedAt(LocalDateTime.now());
+            user.setUpdatedAt(LocalDateTime.now());
+            userRepository.save(user);
+
+            log.info("created default admin account.");
+        }
+
     }
 }
